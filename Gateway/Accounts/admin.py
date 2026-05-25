0271b317c6242bc10cbff51216a6fb2c6ff95c1f
nimamze <nimamze3@gmail.com>
2026-05-25 20:40:09 +0330

    add admin.py

--- Gateway/Accounts/admin.py
+++ Gateway/Accounts/admin.py
@@ -1,3 +1,68 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from Accounts.models import User
+from Accounts.utils.forms import CustomUserCreationForm, CustomUserChangeForm
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = (
+        "phone",
+        "get_user_fullname",
+        "is_seller",
+        "is_superuser",
+        "is_deleted",
+    )
+    list_filter = ("is_superuser", "is_seller", "is_deleted", "is_staff", "is_active")
+    search_fields = ("phone", "email", "first_name", "last_name")
+    ordering = ("phone",)
+    readonly_fields = ("created_at", "updated_at", "last_login", "date_joined")
+
+    add_fieldsets = (
+        ("Personal Info", {"fields": ("phone", "email", "password1", "password2")}),
+        (
+            "Permissions",
+            {"fields": ("is_seller", "is_staff", "is_active", "is_superuser")},
+        ),
+    )
+
+    fieldsets = (
+        (
+            "Personal Info",
+            {
+                "fields": (
+                    "phone",
+                    "email",
+                    "password",
+                    "first_name",
+                    "last_name",
+                    "image",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_seller",
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Dates",
+            {"fields": ("last_login", "date_joined", "created_at", "updated_at")},
+        ),
+        ("Soft Delete", {"fields": ("is_deleted",)}),
+    )
+
+    @admin.display(description="Full Name")
+    def get_user_fullname(self, obj):
+        return obj.get_full_name()
