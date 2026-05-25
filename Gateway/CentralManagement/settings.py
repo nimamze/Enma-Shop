4b1923cccd7f177cf8798926d72d3cd17c497930
nimamze <nimamze3@gmail.com>
2026-05-25 22:22:48 +0330

    add redis and fix bugs

--- Gateway/CentralManagement/settings.py
+++ Gateway/CentralManagement/settings.py
@@ -102,42 +102,44 @@ if DEBUG:
         },
     }
 else:
-    AWS_ACCESS_KEY_ID = os.getenv("ARVAN_S3_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("ARVAN_S3_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = os.getenv(
-        "ARVAN_S3_BUCKET_NAME", "enma-shop-accounts-avatars"
-    )
-    AWS_S3_REGION_NAME = os.getenv("ARVAN_S3_REGION_NAME", "ir-thr-at1")
-    AWS_S3_ENDPOINT_URL = os.getenv(
-        "ARVAN_S3_ENDPOINT_URL", "https://s3.ir-thr-at1.arvanstorage.ir"
-    )
-    AWS_S3_CUSTOM_DOMAIN = os.getenv(
+    custom_domain = os.getenv(
         "ARVAN_S3_CUSTOM_DOMAIN",
-        f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.arvanstorage.ir",
+        f"{os.getenv('ARVAN_S3_BUCKET_NAME')}.s3.{os.getenv('ARVAN_S3_REGION_NAME')}.arvanstorage.ir",
     )
-    AWS_DEFAULT_ACL = None
-    AWS_QUERYSTRING_AUTH = False
-    AWS_S3_FILE_OVERWRITE = False
-
     STORAGES = {
         "default": {
             "BACKEND": "storages.backends.s3.S3Storage",
             "OPTIONS": {
-                "access_key": AWS_ACCESS_KEY_ID,
-                "secret_key": AWS_SECRET_ACCESS_KEY,
-                "bucket_name": AWS_STORAGE_BUCKET_NAME,
-                "region_name": AWS_S3_REGION_NAME,
-                "endpoint_url": AWS_S3_ENDPOINT_URL,
-                "default_acl": AWS_DEFAULT_ACL,
-                "querystring_auth": AWS_QUERYSTRING_AUTH,
-                "file_overwrite": AWS_S3_FILE_OVERWRITE,
-                "custom_domain": AWS_S3_CUSTOM_DOMAIN,
+                "access_key": os.getenv("ARVAN_S3_ACCESS_KEY_ID"),
+                "secret_key": os.getenv("ARVAN_S3_SECRET_ACCESS_KEY"),
+                "bucket_name": os.getenv("ARVAN_S3_BUCKET_NAME"),
+                "region_name": os.getenv("ARVAN_S3_REGION_NAME"),
+                "endpoint_url": os.getenv("ARVAN_S3_ENDPOINT_URL"),
+                "default_acl": None,
+                "querystring_auth": False,
+                "file_overwrite": False,
+                "custom_domain": custom_domain,
             },
         },
         "staticfiles": {
             "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
         },
     }
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
+    MEDIA_URL = f"https://{custom_domain}/"
 
 AUTH_USER_MODEL = "Accounts.User"
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.getenv("REDIS"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {
+                "max_connections": 100,
+                "retry_on_timeout": True,
+            },
+        },
+    }
+}
